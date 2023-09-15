d9ad9f5c1b78e1275807efa74ad5431a637d1446
dkoncius <as.koncius@gmail.com>
2023-09-15 17:17:14 +0300

    undo config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    formats: ['image/avif', 'image/webp', 'image/jpg', 'image/png'],
+    formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com'],
   },
 }
